4daa13c9c180473e15ef1fa5d79f3325c8b22eae
PricingAppAI <pricingapp.ai@gmail.com>
2026-03-12 17:27:20 -0300

    Update market_ai_dashboard.py

--- market_ai_dashboard.py
+++ market_ai_dashboard.py
@@ -448,6 +448,7 @@ if not st.session_state.logged_in:
             user_code = st.text_input("Introduce el código que recibiste por email")
 
             if "verification_sent_time" in st.session_state:
+                st_autorefresh(interval=1000, key="timer")
                 segundos_pasados = time.time() - st.session_state.verification_sent_time
                 espera = 30
                 if segundos_pasados < espera:
