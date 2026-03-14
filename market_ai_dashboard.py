df1ceda25ac19289c0306403de9cae37303b59de
PricingAppAI <pricingapp.ai@gmail.com>
2026-03-13 22:47:50 -0300

    Update market_ai_dashboard.py

--- market_ai_dashboard.py
+++ market_ai_dashboard.py
@@ -621,7 +621,7 @@ if not st.session_state.logged_in:
         <h2 style="color:#ffd700;">$19.999 <span style="color:#cccccc;">/ mes</span></h2>
 
         </div>
-        """, height=240)
+        """, height=300)
 
         st.markdown('<div class="pro-btn">', unsafe_allow_html=True)
 
